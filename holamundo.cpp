827a41f71c5d8da0cb3c641e5fc19a96f9f1fc26
nedder3 <arij.dev@gmail.com>
2023-04-28 18:46:58 -0300

    ADD 1st programm

---
+++ holamundo.cpp
@@ -0,0 +1,19 @@
+//primer programa en c++
+
+//con # include se usa para usar librerias
+
+// iostream : input - output stream . libreria para entradas y salidas de datos
+
+#include<iostream>
+
+//esto es para usar el "package" que contiene cosas como el c out 
+using namespace std; 
+
+int main(){
+	
+	cout<<"Hola mundo :D"<<endl;  //el endl es el equivalente a /n
+
+return 0; //funcion de tipo entero . se pone 0 para decirle al programa que todo termino OK
+
+}
+
