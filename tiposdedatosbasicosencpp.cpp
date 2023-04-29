5c96f6f931ecfc921f4fcf8050136f239c212796
nedder3 <arij.dev@gmail.com>
2023-04-29 13:21:05 -0300

    ADD clase 2

---
+++ tiposdedatosbasicosencpp.cpp
@@ -0,0 +1,20 @@
+//tipos de datos basicos
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int entero =15;
+	float flotante = 10.45;
+	double mayor = 16.3456;
+	
+	//comilla simple no doble
+	char letra ='a';
+	
+	cout<<letra;
+	
+	
+
+return 0;
+}
