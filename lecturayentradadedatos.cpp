0ccdf6d39cb7906235a838730ddabef36621bb30
nedder3 <arij.dev@gmail.com>
2023-04-30 11:42:09 -0300

    clase 03

---
+++ lecturayentradadedatos.cpp
@@ -0,0 +1,27 @@
+//datos I -O
+
+#include<iostream>
+
+using namespace std;
+
+/* Este es un 
+comentario escrito
+en forma de bloque */
+
+
+int main(){
+	int numero;
+	
+	// si o SI COMILLA DOBLE!!
+	
+	cout<<"Digite un numero :" ;
+
+	//cambia la direccion de las flechitas pija 
+	cin>>numero;
+	
+	cout<<"\n el numero que digito es : "<<numero;
+	
+	
+	return 0;
+	
+}
