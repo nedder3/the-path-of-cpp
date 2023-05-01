665962e211b06f77a1b4c1d51af25661d6ee952d
nedder3 <arij.dev@gmail.com>
2023-05-01 14:26:57 -0300

    ADD ejercicio 2 - clase 4

---
+++ Ejercicio2.cpp
@@ -0,0 +1,21 @@
+/*. escribir un programa que Lea de la entrada estandar el precio de un producto y muestre 
+en la salida estandar el precio del producto al aplicarle IVA.*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int precioProducto, iva;
+	
+	iva = 21;
+	
+	cout <<"ingrese el precio del producto : "; cin>>precioProducto;
+
+	precioProducto= precioProducto+(precioProducto*21/100);
+		
+	
+	
+	cout<<"\nEl precio del producto con iva es : "<<precioProducto<<endl;
+
+}
